d85819cfc685df7526ef0e24930b9f9f9c4c974f
atulbhingarde <atul.bhingarde@ge.com>
2019-09-07 22:33:56 -0400

    updated

--- copy_activities.py
+++ copy_activities.py
@@ -1,40 +1,41 @@
 import os
 from pathlib import Path
 from shutil import copyfile
+def stu_activities(rootDir):
+    # set the rootDir where the search will start
+    # that is in home dorectory
+    #rootDir = "Downloads"
+    # build rootDir from rootDir with the base as home "~"
+    rootDir = os.path.join(Path.home(),rootDir)
+    # traverse rootDir and locate the files
+    ####
+    # when the path has Stu_ in it 
+    ####
+    # store currenet directory and build target directory based on current directory 
 
-# set the rootDir where the search will start
-# that is in home dorectory
-rootDir = "Downloads"
-# build rootDir from rootDir with the base as home "~"
-rootDir = os.path.join(Path.home(),rootDir)
-# traverse rootDir and locate the files
-####
-# when the path has Stu_ in it 
-####
-# store currenet directory and build target directory based on current directory 
+    curDir=os.path.curdir
+    targetDir=os.path.join(curDir,"target")
 
-curDir=os.path.curdir
-targetDir=os.path.join(curDir,"target")
-
-print(f'{targetDir}')
-if not os.path.exists(targetDir):
-    os.makedirs(targetDir)
-moveOn = True
-for dirName, subdirList, fileList in os.walk(rootDir):
-    if ( "Stu_" in dirName ):
-     for fname in fileList:
-        sourceFile = os.path.join(dirName,fname)
-        targetFile = os.path.join(targetDir,fname)
-        #print('\t%s' % sourceFile)
-        #print(f'{targetDir}')
-        if not os.path.exists(targetFile):
-            copyfile(sourceFile, targetFile)
-        else:
-            print(f'excuse me I can not copy file {sourceFile} as {targetFile} exists')
-            moveOn = False
-            break
-    else:
-        if ( not moveOn ):
-            break
-    if ( not moveOn ):
-            break
+    print(f'{targetDir}')
+    if not os.path.exists(targetDir):
+        os.makedirs(targetDir)
+    moveOn = True
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        if ( "Stu_" in dirName ):
+            for fname in fileList:
+                sourceFile = os.path.join(dirName,fname)
+                targetFile = os.path.join(targetDir,fname)
+                #print('\t%s' % sourceFile)
+                #print(f'{targetDir}')
+                if not os.path.exists(targetFile):
+                    copyfile(sourceFile, targetFile)
+                else:
+                    print(f'excuse me I can not copy \n source file {sourceFile} as \n target file {targetFile} exists')
+                    moveOn = False
+                    break
+            else:
+                if ( not moveOn ):
+                    break
+            if ( not moveOn ):
+                    break
+stu_activities("Downloads")
