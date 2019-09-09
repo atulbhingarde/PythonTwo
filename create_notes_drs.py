8f44f4533697a27b690bc88681fa463be945317f
atulbhingarde <atul.bhingarde@ge.com>
2019-09-08 20:57:07 -0400

    updated

--- create_notes_drs.py
+++ create_notes_drs.py
@@ -2,10 +2,15 @@ import os
 from pathlib import Path
 from shutil import copyfile
 TopDir="CyberSecurity-Notes"
-
-os.makedirs(TopDir)
+if not os.path.exists(TopDir):
+    os.makedirs(TopDir)
+else:
+    print(f'directory {TopDir} exist I can not proceeed !')
+    exit(1)
 for MyWeeks in range(1,25):
     WeekName = "Week " + str(MyWeeks)
     print(f'{WeekName}')
     for MyDays in range(1,4):
-        print(f'{MyDays}')
+        FolderName= os.path.join(TopDir,WeekName,str(MyDays))
+        # print(f'{FolderName}')
+        os.makedirs(FolderName)
